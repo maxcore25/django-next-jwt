511aa25c93cf725811b00520932152d4673cad6a
maxcore25 <mkorobov2002@mail.ru>
2022-03-03 15:44:22 +0300

    Register View Done

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        try:
+            data = request.data
+
+            first_name = data['first_name']
+            last_name = data['last_name']
+            username = data['username']
+            password = data['password']
+            re_password = data['re_password']
+
+            if password == re_password:
+                if len(password) >= 8:
+                    if not User.objects.filter(username=username).exists():
+                        user = User.objects.create_user(
+                            first_name=first_name,
+                            last_name=last_name,
+                            username=username,
+                            password=password,
+                        )
+                        user.save()
+
+                        if User.objects.filter(username=username).exists():
+                            return Response(
+                                {'error': 'Account created successfully'},
+                                status=status.HTTP_201_CREATED
+                            )
+                        else:
+                            return Response(
+                                {'error': 'Something went wrong while trying to create account'},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                            )
+                    else:
+                        return Response(
+                            {'error': 'Username already exists'},
+                            status=status.HTTP_400_BAD_REQUEST
+                        )
+                else:
+                    return Response(
+                        {'error': 'Password must be at least 8 characters long'},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+            else:
+                return Response(
+                    {'error': 'Passwords do not match'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        except Exception as e:
+            print('Server exception', e)
+            return Response(
+                {'error': 'Something went wrong while trying to register account'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
