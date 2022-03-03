6dc0e8a200220549007d1a638c966e6beeac9878
maxcore25 <mkorobov2002@mail.ru>
2022-03-03 15:52:57 +0300

    Register Account Endpoint Created

--- backend/http_only_auth/urls.py
+++ backend/http_only_auth/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/token/verify/', TokenVerifyView.as_view()),
+    path('api/account/', include('account.urls')),
 ]
