727e51c8e1df45ba2164645cde14f9976b95e231
maxcore25 <mkorobov2002@mail.ru>
2022-03-04 09:14:21 +0300

    Login Page Created

---
+++ frontend/src/pages/dashboard.js
@@ -0,0 +1,11 @@
+import Layout from '../hocs/Layout';
+
+const dashboard = () => {
+  return (
+    <Layout title='httpOnly Auth | Dashboard' content='Nice description'>
+      <h1>Dashboard</h1>
+    </Layout>
+  );
+};
+
+export default dashboard;
---
+++ frontend/src/pages/login.js
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../actions/auth';
+import Layout from '../hocs/Layout';
+import Loader from 'react-loader-spinner';
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const { username, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(dispatch);
+    if (dispatch && dispatch !== null && dispatch !== undefined) {
+      dispatch(
+        register(first_name, last_name, username, password, re_password)
+      );
+    }
+  };
+
+  return (
+    <Layout title='httpOnly Auth | Register' content='Nice description'>
+      <h1>Register Page</h1>
+      <form onSubmit={onSubmit}>
+        <h3>Create an Account</h3>
+        <div>
+          <div>
+            <label htmlFor='first_name'>First name*</label>
+          </div>
+          <input
+            type='text'
+            name='first_name'
+            id='first_name'
+            onChange={onChange}
+            value={first_name}
+            required
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor='last_name'>Last name*</label>
+          </div>
+          <input
+            type='text'
+            name='last_name'
+            id='last_name'
+            onChange={onChange}
+            value={last_name}
+            required
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor='username'>Username*</label>
+          </div>
+          <input
+            type='text'
+            name='username'
+            id='username'
+            onChange={onChange}
+            value={username}
+            required
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor='password'>Password*</label>
+          </div>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            onChange={onChange}
+            value={password}
+            required
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor='re_password'>Repeat Password*</label>
+          </div>
+          <input
+            type='password'
+            name='re_password'
+            id='re_password'
+            onChange={onChange}
+            value={re_password}
+            required
+            minLength={8}
+          />
+        </div>
+        <button type='submit'>Sign Up</button>
+      </form>
+    </Layout>
+  );
+};
+
+export default Login;
